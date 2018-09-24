910d8d50059486d0ddaf263b4e28d2dfac682054
aubreeabril <aubree.abril@gmail.com>
2018-09-24 11:24:35 -0400

    Done.

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,25 @@
 function insertionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  // move through the array
+  for (let i = 1; i < arr.length; i++) {
+    let temp = arr[i];
+    let comp = arr[i - 1];
+    // compare second and first items in the array
+    if (temp < comp) {
+      // swap if necessary
+      temp = comp;
+      comp = arr[i - 1];
+    }
+    // move backward through the array
+    for (let j = i - 0; j > 0; j--) {
+      let tempJ = arr[j];
+      // compare
+      if (tempJ < arr[j - 1]) {
+        // swap
+        arr[j] = arr[j - 1];
+        arr[j - 1] = tempJ;
+      }
+    }
+  }
 }
--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,17 @@
 function selectionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  for (let i = 0; i < arr.length; i++) {
+    let smallestIdx = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[smallestIdx]) {
+        smallestIdx = j;
+      }
+    }
+    if (i !== smallestIdx) {
+      let temp = arr[i];
+      arr[i] = arr[smallestIdx];
+      arr[smallestIdx] = temp;
+    }
+  }
 }
